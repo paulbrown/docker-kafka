1fa89ec2eeb40d3a64a4a8ed98afd587cf9d3fba
paulbrown <paul6rown@gmail.com>
2016-09-08 12:10:15 +1000

    add files

--- onStart.sh
+++ onStart.sh
@@ -5,9 +5,17 @@
 T_HOSTNAME=$(hostname)
 T_ID=$( echo ${T_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
 
+# PetSet index becomes the kafka broker id.
 echo "broker.id="${T_ID} >> /etc/kafka/kafka.properties
+
+# Not ideal to hard code the zookeeper service name but will work for now.
+# TODO: update script to take a parameter that contains the zookeeper service name.
+# Don't think it can be automated, as only the person deploying kafka 
+# will know where the dependent zookeeper service is. 
 echo "zookeeper.connect=zookeeper:2181" >> /etc/kafka/kafka.properties
 
+# Lets use the provided log4j properties.
 export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/etc/kafka/log4j.properties"
 
+# Start kafka container with our properties file.
 exec /usr/bin/kafka-server-start /etc/kafka/kafka.properties
